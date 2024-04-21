9102a7a7287560fe816810f8feb4b3af04732ead
Clément <clement.terme.simplon@gmail.com>
2024-04-21 14:45:04 +0200

    Fichier Config.php nettoyé

--- config.php
+++ config.php
@@ -9,9 +9,3 @@ define('PREFIXE', "fest_");
 define("HOME_URL", "/");
 
 define("DATABASE_INITIALIZED", TRUE);
-
-
-
-// define("DATABASE_NAME", "my_webapp__12");
-// define("DATABASE_USERNAME", "my_webapp__12");
-// define("DATABASE_PASSWORD", "mHlKd1b39tl6WrV6E7tic8kOu8O6ro");
